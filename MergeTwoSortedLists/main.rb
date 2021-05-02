5a690d74de303d8ee67f69d6d7e0ead9cda4ec74
naoki-tsunekawa <b1014138@gmail.com>
2021-05-02 21:14:45 +0900

    MergeTwoSortedLists

---
+++ MergeTwoSortedLists/main.rb
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def merge_two_lists(l1, l2)
+	main_list = []
+
+	# while i < l1.length || k < l2.length
+	#     #リストの要素順に比較
+	#     if l1(i) < l2(k)
+	#         main_list.push(l1(i))
+	#         i += 1
+	#     elsif l1(i) > l2(k)
+	#         main_list.push(l2(k))
+	#         k += 1
+	#     elsif l1(i) == l2(k)
+	#         main_list.push(l2(k))
+	#         main_list.push(l1(i))
+	#         i += 1
+	#         k += 1
+	#     end
+	#     return main_list
+	# end
+
+end
